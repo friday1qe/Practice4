908d1861f2e9da85dc5120316a59addc4a868301
[artyom kosiuk] <[my email]>
2024-10-17 15:53:33 +0200

    adding creatin posts function

--- js/task2.js
+++ js/task2.js
@@ -2,18 +2,67 @@
 
 const { faker } = require('@faker-js/faker')
 
+const selectUserCount = 3;
+let dataOfUsers = [];
+
 class User {
-    constructor() {
-        this.name = faker.person.firstName();
-        // Генеруємо email на основі імені з випадковим доменом
-        this.email = `${this.name.toLowerCase()}@${faker.internet.domainName()}`;
-        // Генеруємо нікнейм на основі імені з додаванням випадкового числа
-        this.nickname = `${this.name.toLowerCase()}${Math.floor(Math.random() * 100) + 1}`;
+    constructor(name, email, nickname, ownPosts) {
+        this.name = name, // faker.person.firstName();
+        this.email = email, // `${this.name.toLowerCase()}@${faker.internet.domainName()}`; // Генеруємо email на основі імені з випадковим доменом
+        this.nickname = nickname // `${this.name.toLowerCase()}${Math.floor(Math.random() * 100) + 1}`; // Генеруємо нікнейм на основі імені з додаванням випадкового числа
+        this.ownPosts = ownPosts;
     }
 }
 
-let users = [new User()];
+class Post {
+    constructor(owner, likes, comments, creationDate) {
+        this.owner = owner,
+        this.liker = likes,
+        this.comments = comments,
+        this.creationDate = creationDate
+    }
+}
 
-users.push(new User());
 
-console.log(users);
+
+function createUser(userCount) {
+    for (let i = 0; i < userCount; i++) {
+        let u = new User();
+        u.name = faker.person.firstName();
+        u.email = `${u.name.toLowerCase()}@${faker.internet.domainName()}`;
+        u.nickname = `${u.name.toLowerCase()}${Math.floor(Math.random() * 100) + 1}`;
+        u.ownPosts = [];
+
+        dataOfUsers.push(u);
+    }
+}
+createUser(selectUserCount, Post);
+
+function postsAdding() {
+    let p = new createPost;
+    p.owner;
+    p.likes = Math.floor(Math.random() * 600);  
+    p.comments = this.nickname;
+}
+
+
+function createPost() {
+    let p = new createPost;
+    p.owner = this.nickname;
+    p.likes = Math.random()*10;  
+    p.comments = this.nickname;
+}
+
+console.log(dataOfUsers[0].ownPosts[0]);
+
+console.log(Math.floor(Math.random() * 100));
+
+// 1 В системі є користувачі
+// 2 кожен користувач має ім'я , імейл та нікнейм ( @nickname) - дані описані в коді
+// 3 у кожного користувача є його пости в соц мережі
+// 4 кожен пост має дату створення, текст поста, кількість лайків , коментарі до посту 
+// 5 користувач може отримати остані 5 постів іншого користувача (вивести в консоль повну інформацію про остані 5 постів)
+// 6 коистувач може поставити лайк чи написати коментар до поста іншого користувача
+// 7 користувач може створити собі новий пост
+// 8 користувач може видалити свій пост
+// 9 Для виконання роботи використовуйте класи. структури та функції для організації коду.
